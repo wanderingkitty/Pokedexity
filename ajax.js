76831f7e853a568a4455d9c6f1bc082ad5e0690a
wanderingkitty <alinaericson95@gmail.com>
2023-12-09 16:30:42 +0100

    added function to a logo-btn to have pokemons names on a list and as a separate divs elemenst for each of them

--- ajax.js
+++ ajax.js
@@ -1,8 +1,24 @@
-async function getPokemon() {
-	const url = 'https://pokeapi.co/api/v2/pokemon/ditto'
-	const response = await fetch(url)
-	const data = await response.json()
-
-	return data.abilities
-}
-export { getPokemon }
+const sample = {
+		"count": 15,
+		"next": null,
+		"previous": null,
+		"results": [
+			{ "name": "ogerpon-hearthflame-mask", "url": "https://pokeapi.co/api/v2/pokemon/10274/" }, 
+			{ "name": "milcery", "url": "https://pokeapi.co/api/v2/pokemon/868/" }, 
+			{ "name": "venusaur-gmax", "url": "https://pokeapi.co/api/v2/pokemon/10195/" }, 
+			{ "name": "sunflora", "url": "https://pokeapi.co/api/v2/pokemon/192/" }, 
+			{ "name": "mimikyu-totem-busted", "url": "https://pokeapi.co/api/v2/pokemon/10145/" }, 
+			{ "name": "simisear", "url": "https://pokeapi.co/api/v2/pokemon/514/" }, 
+			{ "name": "roaring-moon", "url": "https://pokeapi.co/api/v2/pokemon/1005/" }, 
+			{ "name": "gardevoir", "url": "https://pokeapi.co/api/v2/pokemon/282/" }, 
+			{ "name": "munkidori", "url": "https://pokeapi.co/api/v2/pokemon/1015/" }, 
+			{ "name": "pincurchin", "url": "https://pokeapi.co/api/v2/pokemon/871/" }, 
+			{ "name": "cobalion", "url": "https://pokeapi.co/api/v2/pokemon/638/" }, 
+			{ "name": "koraidon-swimming-build", "url": "https://pokeapi.co/api/v2/pokemon/10266/" }, 
+			{ "name": "indeedee-male", "url": "https://pokeapi.co/api/v2/pokemon/876/" }, 
+			{ "name": "suicune", "url": "https://pokeapi.co/api/v2/pokemon/245/" }, 
+			{ "name": "gourgeist-super", "url": "https://pokeapi.co/api/v2/pokemon/10032/" }
+		]
+	}
+	
+export { sample }
--- data.js
+++ data.js
@@ -1,8 +1,29 @@
-import { getPokemon } from './ajax.js';
+import { sample } from './ajax.js';
 
-const activateListOfPokemons = document.querySelector('.logo-btn');
-const pokemonContainer = document.querySelector('.pokemon-container');
+const listOfPokemons = document.querySelector('#poke-list')
+const getListOfPokemonsBtn = document.querySelector('.logo-btn')
 
-activateListOfPokemons.addEventListener('click', async () => {
-	
+async function getPokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const data = await response.json();
+    console.log('Data from API:', data);
+    return data.results;
 }
+
+getListOfPokemonsBtn.addEventListener('click', async () => {
+    try {
+        const pokemonList = await getPokemon();
+
+        listOfPokemons.innerHTML = ''; 
+        pokemonList.forEach(pokemon => {
+            const div = document.createElement('div');
+            div.className = 'pokemon-name';
+            div.textContent = pokemon.name;
+            listOfPokemons.appendChild(div);
+        });
+		
+        addToHistory(pokemonList);
+    } catch (error) {
+        console.log('An error occurred! ' + error.message);
+    }
+});
