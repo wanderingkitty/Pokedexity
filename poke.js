0b0fc39940893d01f636f66122a64f21d53bd1db
wanderingkitty <alinaericson95@gmail.com>
2023-12-18 19:50:13 +0100

    fixed grid layout and added click function to add pokemons

--- poke.js
+++ poke.js
@@ -6,13 +6,15 @@ const pokemonListUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
 const displayLimit = 30; 
 export const searchPokemonInput = document.querySelector('#site-search');
 let allPokemonData = [];
+const myTeam = []
+const maxTeamMembers = 3
+
 
 // Function to hide second view screen
 export function hideSecondScreen() {
 	console.log('Hiding second screen');
 	firstScreen.classList.remove('hide');
 	firstScreen.classList.add('show');
-	
 	secondScreen.classList.remove('show');
 	secondScreen.classList.add('hide');
 }
@@ -47,15 +49,17 @@ function searchPokemon() {
 			const pokemonCard = document.createElement('div');
 			pokemonCard.className = 'pokemon-list-container';
 			
+			if (pokemonList.length === 1) {
+				pokemonCard.classList.add('centered-pokemon'); 
+			}
+			
 			const img = document.createElement('img');
-			// Check multiple image sources
 			img.src = detailedPokemon.sprites.other.dream_world.front_default ||
 			detailedPokemon.sprites.other['official-artwork'].front_default ||
 			detailedPokemon.sprites.front_default ||
 			'/img/poketext.png'
 			
-			
-			const pokemonInfo = document.createElement('div'); // Create the pokemonInfo div
+			const pokemonInfo = document.createElement('div'); 
 			pokemonInfo.className = 'pokemon-info';
 			
 			const pokemonName = document.createElement('div');
@@ -88,21 +92,59 @@ function searchPokemon() {
 			//Gettingpokemon types
 			const pokemonTypes = document.createElement('div');
 			pokemonTypes.className = 'pokemon-types';
-	
+			
 			detailedPokemon.types.forEach((typeSlot) => {
 				const type = document.createElement('span');
 				const typeName = typeSlot.type.name;
-	
-				// Use a ternary operator to set the background color
+				
 				type.style.backgroundColor = colors[typeName] || '#CCCCCC';
-	
+				
 				type.className = 'pokemon-type';
 				type.textContent = typeName;
 				pokemonTypes.appendChild(type);
 			});
-	
+			
+			const teamList = document.querySelector('.team-list');
+			//Function to get added pokemons on a separate list
+			function updateTeamList() {
+				teamList.innerHTML = ''; 
+				
+				myTeam.forEach((pokemonData) => {
+					const teamPokemonCard = document.createElement('div');
+					teamPokemonCard.className = 'team-pokemon-card';
+					
+					const teamPokemonName = document.createElement('div');
+					teamPokemonName.textContent = pokemonData.name.toUpperCase();
+					teamPokemonName.className = 'pokemon-name';
+					
+					teamPokemonCard.appendChild(teamPokemonName);
+					teamList.appendChild(teamPokemonCard);
+				});
+			}
+			updateTeamList()
+			
 			pokemonInfo.appendChild(pokemonTypes);
 			pokemonCard.appendChild(pokemonInfo);
+			
+			let popUpMessege = document.createElement('div')
+			popUpMessege.innerText = 'Team is full'
+			
+			//Click event to add pokemon to the team
+			addButton.addEventListener('click', () => {
+				if (myTeam.length < maxTeamMembers) { 
+					if (!myTeam.includes(detailedPokemon)) {
+						myTeam.push(detailedPokemon);
+						console.log(`Added ${detailedPokemon.name} to the team`);
+	
+						updateTeamList();
+					} else {
+						console.log(`${detailedPokemon.name} is already in the team`);
+					}
+				} else {
+					console.log('Cannot add more members. Team is full.');
+					document.body.appendChild(popUpMessege);
+				}
+			});
 		});
 	}
 	
@@ -110,7 +152,7 @@ function searchPokemon() {
 	export async function getPokemonDetails() {
 		try {
 			const response = await getPokemon(pokemonListUrl);
-			allPokemonData = response.results; // Store all Pokemon data
+			allPokemonData = response.results;
 			shuffleArray(allPokemonData);
 			displayPokemon(allPokemonData.slice(0, displayLimit));
 		} catch (error) {
@@ -129,6 +171,5 @@ function searchPokemon() {
 	}
 	
 	searchPokemonInput.addEventListener('input', debounce(searchPokemon, 300));
-	
-	
 	document.addEventListener('DOMContentLoaded', getPokemonDetails);
+	
