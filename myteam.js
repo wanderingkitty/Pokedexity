3c26c28dee382a573437e563bcb8fe707f933e59
wanderingkitty <alinaericson95@gmail.com>
2023-12-20 20:45:15 +0100

    fixed files location

--- myteam.js
+++
@@ -1,9 +0,0 @@
-export const myTeam = document.querySelector('.team-btn')
-import { secondScreen } from './poke.js'
-
-myTeam.addEventListener('click', () => {
-	firstScreen.classList.add('hide');
-	firstScreen.classList.remove('show');
-	secondScreen.classList.add('show');
-	secondScreen.classList.remove('hide');
-})
